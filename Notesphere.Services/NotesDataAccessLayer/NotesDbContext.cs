f5f74f8425cae57359618450f16d1c3f1d7a1265
Mamin Khan <khanmami@sheridancollege.ca>
2025-11-25 18:19:45 -0500

    created NotesDbcontext class + added the project references

--- Notesphere.Services/NotesDataAccessLayer/NotesDbContext.cs
+++ Notesphere.Services/NotesDataAccessLayer/NotesDbContext.cs
@@ -1,8 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Notesphere.Entities.NotesModels;
 namespace Notesphere.Services.NotesDataAccessLayer
 {
-    public class NotesDbContext
+    public class NotesDbContext:DbContext
     {
+        public NotesDbContext(DbContextOptions<NotesDbContext> options) : base(options)
+        {
+        }
 
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<NoteTag> NoteTags { get; set; }
+        public DbSet<NoteTemplate> NoteTemplates { get; set; }
+        public DbSet<NoteVersion> NoteVersions { get; set; }
+        public DbSet<NoteExport> NoteExports { get; set; }
     }
 }
